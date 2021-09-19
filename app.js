1318621ccc58958f311c38ff2f4244650af4acad
Frangky <81846311+Franky404@users.noreply.github.com>
2021-09-19 08:50:43 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+
+//Recode by YogiPw
+const express = require('express');
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage
+}).single('recfile');
+
+
+// Init app
+const app = express();
+
+// EJS
+app.set('view engine', 'ejs');
+
+// Public Folder
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      res.render('index', {
+        msg: err
+      });
+    } else {
+      if(req.file == undefined){
+        res.render('index', {
+          msg: 'Error: No File Selected!'
+        });
+      } else {
+        res.json({
+          author: 'YogiPw', 
+          msg: 'File Uploaded!',
+          file: `https://nodeuploads.yogipw.repl.co/uploads/${req.file.filename}`
+        });
+      }
+    }
+  });
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
